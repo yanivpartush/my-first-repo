db9ef64d48ee2ce2a3890110fb53ad3854099b97
Yaniv P <yanivpartush@gmail.com>
2025-04-08 00:18:08 +0300

    student details changes

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("Alex", 30, "English", 3 ,85)
+student = Student("Yaniv", 59, "Math", 3 ,90)
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
