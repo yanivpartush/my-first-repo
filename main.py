bd557a4f3c9e8f06b2cc2079f73ffd31e0214f80
Yaniv P <yanivpartush@gmail.com>
2025-04-08 00:27:49 +0300

    Fixed bug, missing abg score

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("Alex", 30, "English", 3 )
+student = Student("Alex", 30, "English", 3 ,70)
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
