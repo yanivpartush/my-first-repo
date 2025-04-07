22d88aa6bc36de142e284dd6cb15a6d326d729a1
Yaniv P <yanivpartush@gmail.com>
2025-04-08 00:09:55 +0300

    Change student to a diff one

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from Student import Student
 from Employee import Employee
 
-student = Student("Alex", 30, "English", 3 ,85)
+student = Student("Alex", 30, "English", 3 )
 # student.foo()
 
 employee = Employee("John", 40, "Software Engineer", 45000)
